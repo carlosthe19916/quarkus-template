a3d0fd04767e3294944bcaa00e9a6dce0e9207fb
Carlos Esteban Feria Vila <carlosthe19916@gmail.com>
2019-10-10 20:43:18 +0200

    add quarkus

---
+++ src/test/java/io/github/carlosthe19916/NativeGreetingResourceIT.java
@@ -0,0 +1,9 @@
+package io.github.carlosthe19916;
+
+import io.quarkus.test.junit.SubstrateTest;
+
+@SubstrateTest
+public class NativeGreetingResourceIT extends GreetingResourceTest {
+
+    // Execute the same tests but in native mode.
+}
